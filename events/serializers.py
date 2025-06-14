898451e6555ed4cc2394523ae38273a18c04af89
Mmy2000 <mm.yousef811@gmail.com>
2025-06-14 22:52:45 +0300

    feat: starting with event app

--- events/serializers.py
+++ events/serializers.py
@@ -10,7 +10,21 @@ class EventSerializer(serializers.ModelSerializer):
     creator = SampleUserData(read_only=True)
     attendees = SampleUserData(many=True, read_only=True)
     interested_users = SampleUserData(many=True, read_only=True)
+    is_joined = serializers.SerializerMethodField()
+    is_interested = serializers.SerializerMethodField()
 
     class Meta:
         model = Event
         fields = "__all__"
+
+    def get_is_joined(self, obj):
+        request = self.context.get("request")
+        if request and request.user.is_authenticated:
+            return request.user in obj.attendees.all()
+        return False
+    
+    def get_is_interested(self, obj):
+        request = self.context.get("request")
+        if request and request.user.is_authenticated:
+            return request.user in obj.interested_users.all()
+        return False
